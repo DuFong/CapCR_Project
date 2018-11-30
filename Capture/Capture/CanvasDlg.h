a4f3ff90eabd1bee6db73b7d67fd0582c60dc7a3
yunakim2 <dbskzla0677@gmail.com>
2018-11-30 17:59:47 +0900

    //시작

---
+++ Capture/Capture/CanvasDlg.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#define WS_EX_LAYERED  0x00080000  //Dialog Alpha define
+#define LWA_ALPHA   0x00000002
+// CanvasDlg 대화 상자입니다.
+
+class CanvasDlg : public CDialog
+{
+	DECLARE_DYNAMIC(CanvasDlg)
+
+public:
+	CanvasDlg(CWnd* pParent = NULL);   // 표준 생성자입니다.
+	virtual ~CanvasDlg();
+
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_CANVAS };
+#endif
+
+protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV 지원입니다.
+
+	DECLARE_MESSAGE_MAP()
+public:
+	LONG m_OrgX;
+	LONG m_OrgY;
+	LONG m_DstX;
+	LONG m_DstY;
+	virtual BOOL OnInitDialog();
+	afx_msg void OnLButtonDown(UINT nFlags, CPoint point);
+	afx_msg void OnLButtonUp(UINT nFlags, CPoint point);
+	afx_msg void OnMouseMove(UINT nFlags, CPoint point);
+	afx_msg void OnKillFocus(CWnd* pNewWnd);
+	afx_msg void OnPaint();
+	afx_msg HBRUSH OnCtlColor(CDC* pDC, CWnd* pWnd, UINT nCtlColor);
+	RECT GetClipRect();
+};
---
+++ Capture/Capture/CaptureDlg.cpp
@@ -0,0 +1,231 @@
+
+// CaptureDlg.cpp : 구현 파일
+//
+
+#include "stdafx.h"
+#include "Capture.h"
+#include "CaptureDlg.h"
+#include "afxdialogex.h"
+#include "CanvasDlg.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// 응용 프로그램 정보에 사용되는 CAboutDlg 대화 상자입니다.
+
+
+
+
+class CAboutDlg : public CDialogEx
+{
+public:
+	CAboutDlg();
+
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_ABOUTBOX };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV 지원입니다.
+
+// 구현입니다.
+protected:
+	DECLARE_MESSAGE_MAP()
+};
+
+CAboutDlg::CAboutDlg() : CDialogEx(IDD_ABOUTBOX)
+{
+}
+
+void CAboutDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+}
+
+BEGIN_MESSAGE_MAP(CAboutDlg, CDialogEx)
+END_MESSAGE_MAP()
+
+
+// CCaptureDlg 대화 상자
+
+
+
+CCaptureDlg::CCaptureDlg(CWnd* pParent /*=NULL*/)
+	: CDialogEx(IDD_CAPTURE_DIALOG, pParent)
+{
+	m_hIcon = AfxGetApp()->LoadIcon(IDR_MAINFRAME);
+}
+
+void CCaptureDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+	
+}
+
+BEGIN_MESSAGE_MAP(CCaptureDlg, CDialogEx)
+	ON_WM_SYSCOMMAND()
+	ON_WM_PAINT()
+	ON_WM_QUERYDRAGICON()
+	ON_BN_CLICKED(IDC_BUTTON_CAPTURE, &CCaptureDlg::OnClickedButtonCapture)
+
+END_MESSAGE_MAP()
+
+
+// CCaptureDlg 메시지 처리기
+
+BOOL CCaptureDlg::OnInitDialog()
+{
+	CDialogEx::OnInitDialog();
+
+	// 시스템 메뉴에 "정보..." 메뉴 항목을 추가합니다.
+
+	// IDM_ABOUTBOX는 시스템 명령 범위에 있어야 합니다.
+	ASSERT((IDM_ABOUTBOX & 0xFFF0) == IDM_ABOUTBOX);
+	ASSERT(IDM_ABOUTBOX < 0xF000);
+
+	CMenu* pSysMenu = GetSystemMenu(FALSE);
+	if (pSysMenu != NULL)
+	{
+		BOOL bNameValid;
+		CString strAboutMenu;
+		bNameValid = strAboutMenu.LoadString(IDS_ABOUTBOX);
+		ASSERT(bNameValid);
+		if (!strAboutMenu.IsEmpty())
+		{
+			pSysMenu->AppendMenu(MF_SEPARATOR);
+			pSysMenu->AppendMenu(MF_STRING, IDM_ABOUTBOX, strAboutMenu);
+		}
+	}
+
+	// 이 대화 상자의 아이콘을 설정합니다.  응용 프로그램의 주 창이 대화 상자가 아닐 경우에는
+	//  프레임워크가 이 작업을 자동으로 수행합니다.
+	SetIcon(m_hIcon, TRUE);			// 큰 아이콘을 설정합니다.
+	SetIcon(m_hIcon, FALSE);		// 작은 아이콘을 설정합니다.
+
+	// TODO: 여기에 추가 초기화 작업을 추가합니다.
+
+	return TRUE;  // 포커스를 컨트롤에 설정하지 않으면 TRUE를 반환합니다.
+}
+
+void CCaptureDlg::OnSysCommand(UINT nID, LPARAM lParam)
+{
+	if ((nID & 0xFFF0) == IDM_ABOUTBOX)
+	{
+		CAboutDlg dlgAbout;
+		dlgAbout.DoModal();
+	}
+	else
+	{
+		CDialogEx::OnSysCommand(nID, lParam);
+	}
+}
+
+// 대화 상자에 최소화 단추를 추가할 경우 아이콘을 그리려면
+//  아래 코드가 필요합니다.  문서/뷰 모델을 사용하는 MFC 응용 프로그램의 경우에는
+//  프레임워크에서 이 작업을 자동으로 수행합니다.
+
+void CCaptureDlg::OnPaint()
+{
+	if (IsIconic())
+	{
+		CPaintDC dc(this); // 그리기를 위한 디바이스 컨텍스트입니다.
+
+		SendMessage(WM_ICONERASEBKGND, reinterpret_cast<WPARAM>(dc.GetSafeHdc()), 0);
+
+		// 클라이언트 사각형에서 아이콘을 가운데에 맞춥니다.
+		int cxIcon = GetSystemMetrics(SM_CXICON);
+		int cyIcon = GetSystemMetrics(SM_CYICON);
+		CRect rect;
+		GetClientRect(&rect);
+		int x = (rect.Width() - cxIcon + 1) / 2;
+		int y = (rect.Height() - cyIcon + 1) / 2;
+
+		// 아이콘을 그립니다.
+		dc.DrawIcon(x, y, m_hIcon);
+	}
+	else
+	{
+		CDialogEx::OnPaint();
+	}
+}
+
+// 사용자가 최소화된 창을 끄는 동안에 커서가 표시되도록 시스템에서
+//  이 함수를 호출합니다.
+HCURSOR CCaptureDlg::OnQueryDragIcon()
+{
+	return static_cast<HCURSOR>(m_hIcon);
+}
+
+
+
+void CCaptureDlg::OnClickedButtonCapture()
+{
+	if (Image != NULL)
+		Image.Destroy();
+
+
+	CString imgName = _T("Desktop.jpg");
+	// TODO: 여기에 컨트롤 알림 처리기 코드를 추가합니다.
+	CanvasDlg canvas;
+	canvas.DoModal();
+
+	RECT area = canvas.GetClipRect();
+	int nClipWidth = area.right - area.left;
+	int nClipHeight = area.bottom - area.top;
+
+	CWnd* pWndDesktop = GetDesktopWindow();
+	// 바탕 화면 윈도우 DC 
+
+	CWindowDC ScrDC(pWndDesktop);
+	// 뷰 윈도우 DC
+	CClientDC dc(this);
+	CRect Rect;
+	GetClientRect(&Rect);
+	CWnd::GetWindowRect(&Rect);
+	
+
+	int sx = area.left;
+	int sy = area.top;
+	int cx = nClipWidth;
+	int cy = nClipHeight;
+
+	Image.Create(cx, cy, ScrDC.GetDeviceCaps(BITSPIXEL));
+
+	// 이미지 DC에 현재 작업 영역의 절대 좌표를 사용해 그 크기만큼 저장하게 한다. 
+
+	CDC* pDC = CDC::FromHandle(Image.GetDC());
+	pDC->BitBlt(0, 0, cx, cy, &ScrDC, sx, sy, SRCCOPY);
+	Image.ReleaseDC();
+
+
+	// 저장된 이미지를 원하는 파일명, 포멧타입을 지정해서 저장한다. 
+
+	// Image.Save(imgName, Gdiplus::ImageFormatJPEG);
+
+    
+     //CStatic *staticSize = (CStatic*)GetDlgItem(AFX_IDC_PICTURE);
+    // CRect rect2;
+
+	// staticSize->GetClientRect(rect2);
+	   //Image.Load(imgName);
+	
+	Image.BitBlt(dc.m_hDC, 0, 0);
+	
+	Image.Save(imgName, Gdiplus::ImageFormatJPEG);
+
+
+	   // 그 파일을 실행해 준다. 
+
+	
+	/*::ShellExecute(NULL,
+		TEXT("open"), TEXT("Desktop.jpg"),
+		NULL, NULL, SW_SHOW);
+		*/
+		
+
+}
+
+
