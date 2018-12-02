8153ee3a04a58834126e59d6e52ad6f61eb23be5
ㅇㅣㅇㅕㅇㅈㅐ <youngjewall@me.com>
2018-12-03 01:19:14 +0900

    [NEW] CapCR

---
+++ CapCR/CapCR/CanvasDlg.cpp
@@ -0,0 +1,190 @@
+// CanvasDlg.cpp : 구현 파일입니다.
+//
+
+#include "stdafx.h"
+#include "CapCR.h"
+#include "CanvasDlg.h"
+#include "afxdialogex.h"
+
+
+// CanvasDlg 대화 상자입니다.
+
+IMPLEMENT_DYNAMIC(CanvasDlg, CDialog)
+
+CanvasDlg::CanvasDlg(CWnd* pParent /*=NULL*/)
+	: CDialog(IDD_CANVAS, pParent)
+	, m_OrgX(0)
+	, m_OrgY(0)
+	, m_DstX(0)
+	, m_DstY(0)
+	, m_bLButtonDowned(false)
+{
+
+}
+
+CanvasDlg::~CanvasDlg()
+{
+}
+
+void CanvasDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialog::DoDataExchange(pDX);
+}
+
+
+BEGIN_MESSAGE_MAP(CanvasDlg, CDialog)
+	ON_WM_LBUTTONDOWN()
+	ON_WM_LBUTTONUP()
+	ON_WM_MOUSEMOVE()
+	ON_WM_KILLFOCUS()
+	ON_WM_PAINT()
+	ON_WM_CTLCOLOR()
+END_MESSAGE_MAP()
+
+
+// CanvasDlg 메시지 처리기입니다.
+
+
+BOOL CanvasDlg::OnInitDialog()
+{
+	int nScreenWidth = GetSystemMetrics(SM_CXVIRTUALSCREEN);
+	int nScreenHeight = GetSystemMetrics(SM_CYVIRTUALSCREEN);
+	MoveWindow(0, 0, nScreenWidth, nScreenHeight);
+	typedef BOOL(WINAPI *SLWA)(HWND, COLORREF, BYTE, DWORD);
+	SLWA pSetLayeredWindowAttributes = NULL;
+	HWND hwnd = this->m_hWnd;
+
+	SetWindowLong(hwnd, GWL_EXSTYLE, GetWindowLong(hwnd, GWL_EXSTYLE) | WS_EX_LAYERED);
+	SetLayeredWindowAttributes(0, 150, LWA_ALPHA);
+
+	// 최상위 윈도우
+	::SetWindowPos(GetSafeHwnd(), HWND_TOPMOST, 0, 0, 0, 0, SWP_NOMOVE | SWP_NOSIZE);
+
+	// TODO:  여기에 추가 초기화 작업을 추가합니다.
+	
+
+	CDialog::OnInitDialog();
+	return TRUE;  // return TRUE unless you set the focus to a control
+				  // 예외: OCX 속성 페이지는 FALSE를 반환해야 합니다.
+}
+
+
+void CanvasDlg::OnLButtonDown(UINT nFlags, CPoint point)
+{
+
+	
+	// TODO: 여기에 메시지 처리기 코드를 추가 및/또는 기본값을 호출합니다.
+	m_OrgX = m_DstX = point.x;
+	m_OrgY = m_DstY = point.y;
+	if (!m_bLButtonDowned)
+		m_bLButtonDowned = true;
+
+	CDialog::OnLButtonDown(nFlags, point);
+}
+
+
+void CanvasDlg::OnLButtonUp(UINT nFlags, CPoint point)
+{
+	// TODO: 여기에 메시지 처리기 코드를 추가 및/또는 기본값을 호출합니다.
+	m_DstX = point.x;
+	m_DstY = point.y;
+
+	if (m_bLButtonDowned)
+		m_bLButtonDowned = false;
+	CDialog::OnLButtonUp(nFlags, point);
+	::SendMessage(this->m_hWnd, WM_CLOSE, NULL, NULL);
+
+}
+
+
+void CanvasDlg::OnMouseMove(UINT nFlags, CPoint point)
+{
+	// TODO: 여기에 메시지 처리기 코드를 추가 및/또는 기본값을 호출합니다.
+	m_DstX = point.x;
+	m_DstY = point.y;
+	Invalidate(TRUE);
+	CDialog::OnMouseMove(nFlags, point);
+}
+
+
+void CanvasDlg::OnKillFocus(CWnd* pNewWnd)
+{
+	CDialog::OnKillFocus(pNewWnd);
+
+	// TODO: 여기에 메시지 처리기 코드를 추가합니다.
+
+	CDialog::OnKillFocus(pNewWnd);
+
+	// TODO: 여기에 메시지 처리기 코드를 추가합니다.
+	// 키를 입력하면 Canvas 를 숨김(취소)	
+	::SendMessage(this->m_hWnd, WM_CLOSE, NULL, NULL);
+}
+
+
+void CanvasDlg::OnPaint()
+{
+	CPaintDC dc(this); // device context for painting
+					   // TODO: 여기에 메시지 처리기 코드를 추가합니다.
+					   // 그리기 메시지에 대해서는 CDialog::OnPaint()을(를) 호출하지 마십시오
+	if (m_bLButtonDowned)
+	{
+		CPen pen;
+		pen.CreatePen(PS_SOLID, 1, RGB(255, 0, 0));
+		dc.SelectObject(&pen);
+
+		LONG x1 = m_OrgX, x2 = m_DstX, y1 = m_OrgY, y2 = m_DstY;
+		if (x1 > x2) { x1 = m_DstX; x2 = m_OrgX; }
+		if (y1 > y2) { y1 = m_DstY; y2 = m_OrgY; }
+
+
+		dc.Rectangle(x1, y1, x2, y2);
+
+		CString pixelInfo;
+		pixelInfo.Format(_T("%d x %d"), abs(x2 - x1), abs(y2 - y1));
+		RECT textPosition = { x2 - 80, y2, x2, y2 + 15 };
+		DrawText(dc, pixelInfo, -1, &textPosition, DT_VCENTER | DT_SINGLELINE);
+	}
+	// 그리기 메시지에 대해서는 CDialog::OnPaint()을(를) 호출하지 마십시오.
+
+
+
+	
+}
+
+
+HBRUSH CanvasDlg::OnCtlColor(CDC* pDC, CWnd* pWnd, UINT nCtlColor)
+{
+	HBRUSH hbr = CDialog::OnCtlColor(pDC, pWnd, nCtlColor);
+
+	// TODO:  여기서 DC의 특성을 변경합니다.
+	switch (nCtlColor) {
+	case CTLCOLOR_DLG:
+		hbr = (HBRUSH)GetStockObject(WHITE_BRUSH);
+		break;
+	}
+	// TODO:  기본값이 적당하지 않으면 다른 브러시를 반환합니다.
+	return hbr;
+	// TODO:  기본값이 적당하지 않으면 다른 브러시를 반환합니다.
+
+}
+
+
+RECT CanvasDlg::GetClipRect()
+{
+	RECT rect;
+	if (m_OrgX == m_DstX || m_OrgY == m_DstY) {
+		rect.bottom = rect.left = rect.right = rect.top = 0;
+		return rect;
+	}
+
+	LONG x1 = m_OrgX, x2 = m_DstX, y1 = m_OrgY, y2 = m_DstY;
+	if (x1 > x2) { x1 = m_DstX; x2 = m_OrgX; }
+	if (y1 > y2) { y1 = m_DstY; y2 = m_OrgY; }
+
+	rect.left = x1;
+	rect.right = x2;
+	rect.top = y1;
+	rect.bottom = y2;
+
+	return rect;
+}
---
+++ CapCR/CapCR/CapCRView.cpp
@@ -0,0 +1,247 @@
+// 이 MFC 샘플 소스 코드는 MFC Microsoft Office Fluent 사용자 인터페이스("Fluent UI")를 
+// 사용하는 방법을 보여 주며, MFC C++ 라이브러리 소프트웨어에 포함된 
+// Microsoft Foundation Classes Reference 및 관련 전자 문서에 대해 
+// 추가적으로 제공되는 내용입니다.  
+// Fluent UI를 복사, 사용 또는 배포하는 데 대한 사용 약관은 별도로 제공됩니다.  
+// Fluent UI 라이선싱 프로그램에 대한 자세한 내용은 
+// http://go.microsoft.com/fwlink/?LinkId=238214.
+//
+// Copyright (C) Microsoft Corporation
+// All rights reserved.
+
+// CapCRView.cpp : CCapCRView 클래스의 구현
+//
+
+#include "stdafx.h"
+// SHARED_HANDLERS는 미리 보기, 축소판 그림 및 검색 필터 처리기를 구현하는 ATL 프로젝트에서 정의할 수 있으며
+// 해당 프로젝트와 문서 코드를 공유하도록 해 줍니다.
+#ifndef SHARED_HANDLERS
+#include "CapCR.h"
+#endif
+
+#include "CapCRDoc.h"
+#include "CapCRView.h"
+#include "MainFrm.h"
+#include "CanvasDlg.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CCapCRView
+
+IMPLEMENT_DYNCREATE(CCapCRView, CFormView)
+
+BEGIN_MESSAGE_MAP(CCapCRView, CFormView)
+	ON_WM_CONTEXTMENU()
+	ON_WM_RBUTTONUP()
+//	ON_BN_CLICKED(IDC_BUTTON_CAPTURE, &CCapCRView::OnClickedButtonCapture)
+	ON_WM_PAINT()
+	ON_COMMAND(ID_BUTTON_CAPTURE, &CCapCRView::OnButtonCapture)
+END_MESSAGE_MAP()
+
+// CCapCRView 생성/소멸
+
+CCapCRView::CCapCRView()
+	: CFormView(IDD_CAPCR_FORM)
+{
+	// TODO: 여기에 생성 코드를 추가합니다.
+
+}
+
+CCapCRView::~CCapCRView()
+{
+}
+
+void CCapCRView::DoDataExchange(CDataExchange* pDX)
+{
+	CFormView::DoDataExchange(pDX);
+}
+
+BOOL CCapCRView::PreCreateWindow(CREATESTRUCT& cs)
+{
+	// TODO: CREATESTRUCT cs를 수정하여 여기에서
+	//  Window 클래스 또는 스타일을 수정합니다.
+
+	return CFormView::PreCreateWindow(cs);
+}
+
+void CCapCRView::OnInitialUpdate()
+{
+	CFormView::OnInitialUpdate();
+	GetParentFrame()->RecalcLayout();
+	ResizeParentToFit();
+
+}
+
+void CCapCRView::OnRButtonUp(UINT /* nFlags */, CPoint point)
+{
+	ClientToScreen(&point);
+	OnContextMenu(this, point);
+}
+
+void CCapCRView::OnContextMenu(CWnd* /* pWnd */, CPoint point)
+{
+#ifndef SHARED_HANDLERS
+	theApp.GetContextMenuManager()->ShowPopupMenu(IDR_POPUP_EDIT, point.x, point.y, this, TRUE);
+#endif
+}
+
+
+// CCapCRView 진단
+
+#ifdef _DEBUG
+void CCapCRView::AssertValid() const
+{
+	CFormView::AssertValid();
+}
+
+void CCapCRView::Dump(CDumpContext& dc) const
+{
+	CFormView::Dump(dc);
+}
+
+CCapCRDoc* CCapCRView::GetDocument() const // 디버그되지 않은 버전은 인라인으로 지정됩니다.
+{
+	ASSERT(m_pDocument->IsKindOf(RUNTIME_CLASS(CCapCRDoc)));
+	return (CCapCRDoc*)m_pDocument;
+}
+#endif //_DEBUG
+
+
+// CCapCRView 메시지 처리기
+
+
+
+void CCapCRView::OnPaint()
+{
+	CPaintDC dc(this);
+	if (IsIconic())
+	{
+		// 그리기를 위한 디바이스 컨텍스트입니다.
+
+		SendMessage(WM_ICONERASEBKGND, reinterpret_cast<WPARAM>(dc.GetSafeHdc()), 0);
+
+		// 클라이언트 사각형에서 아이콘을 가운데에 맞춥니다.
+		int cxIcon = GetSystemMetrics(SM_CXICON);
+		int cyIcon = GetSystemMetrics(SM_CYICON);
+		CRect rect;
+		GetClientRect(&rect);
+		int x = (rect.Width() - cxIcon + 1) / 2;
+		int y = (rect.Height() - cyIcon + 1) / 2;
+
+		// 아이콘을 그립니다.
+		dc.DrawIcon(x, y, m_hIcon);
+		if (Image.IsNull() == FALSE)			// 창 변화시 그림 사라지는거 방지
+			Image.BitBlt(dc.m_hDC, 0, 0);
+	}
+	else
+	{
+		if (Image.IsNull() == FALSE)		// 창 변화시 그림 사라지는거 방지
+			Image.BitBlt(dc.m_hDC, 0, 0);
+		CFormView::OnPaint();
+	}
+}
+
+
+void CCapCRView::OnButtonCapture()
+{
+	// TODO: 여기에 명령 처리기 코드를 추가합니다.
+	// TODO: 여기에 컨트롤 알림 처리기 코드를 추가합니다.
+	if (Image != NULL)
+		Image.Destroy();
+
+
+	CString imgName = _T("Desktop.jpg");
+	CMainFrame* pFrame = (CMainFrame*)AfxGetMainWnd();
+
+	// TODO: 여기에 컨트롤 알림 처리기 코드를 추가합니다.
+
+	// 현재 창 투명하게
+	::SetWindowLong(pFrame->GetSafeHwnd(), GWL_EXSTYLE, ::GetWindowLong(pFrame->GetSafeHwnd(), GWL_EXSTYLE) | WS_EX_LAYERED);
+	pFrame->SetLayeredWindowAttributes(0, 0, LWA_ALPHA);
+
+
+	// Canvas 다이얼로그 호출
+	CanvasDlg canvas;
+	canvas.DoModal();
+
+
+
+
+	area = canvas.GetClipRect();
+	int nClipWidth = area.right - area.left;
+	int nClipHeight = area.bottom - area.top;
+
+	CWnd* pWndDesktop = GetDesktopWindow();
+	// 바탕 화면 윈도우 DC 
+
+	CWindowDC ScrDC(pWndDesktop);
+	// 뷰 윈도우 DC
+	CClientDC dc(this);
+	CRect Rect;
+	GetClientRect(&Rect);
+	CWnd::GetWindowRect(&Rect);
+
+
+	int sx = area.left;
+	int sy = area.top;
+	int cx = nClipWidth;
+	int cy = nClipHeight;
+
+
+
+	Image.Create(cx, cy, ScrDC.GetDeviceCaps(BITSPIXEL));
+
+	// 이미지 DC에 현재 작업 영역의 절대 좌표를 사용해 그 크기만큼 저장하게 한다. 
+
+	CDC* pDC = CDC::FromHandle(Image.GetDC());
+	pDC->BitBlt(0, 0, cx, cy, &ScrDC, sx, sy, SRCCOPY);
+	Image.ReleaseDC();
+
+	// 저장된 이미지를 원하는 파일명, 포멧타입을 지정해서 저장한다. 
+
+	//Image.Save(imgName, Gdiplus::ImageFormatJPEG);
+
+
+	//CStatic *staticSize = (CStatic*)GetDlgItem(AFX_IDC_PICTURE);
+	// CRect rect2;
+
+	// staticSize->GetClientRect(rect2);
+	//Image.Load(imgName);
+
+	SIZE s;
+	ZeroMemory(&s, sizeof(SIZE));
+	s.cx = (LONG)::GetSystemMetrics(SM_CXFULLSCREEN);
+	s.cy = (LONG)::GetSystemMetrics(SM_CYFULLSCREEN);
+
+	Image.Save(imgName, Gdiplus::ImageFormatJPEG);
+
+	if (cx > 520 && cy > 300)
+		pFrame->SetWindowPos(NULL, (s.cx / 2) - cx / 2, 0, cx + 50, cy + 200, SWP_NOREPOSITION);
+	else if (cx < 520 && cy < 300)
+		pFrame->SetWindowPos(NULL, s.cx / 2 - 260, 0, 570, 500, SWP_NOREPOSITION);
+	else if (cx < 520)
+		pFrame->SetWindowPos(NULL, (s.cx / 2) - 280, 0, 570, cy + 200, SWP_NOREPOSITION);
+	else
+		pFrame->SetWindowPos(NULL, s.cx / 2 - (cx / 2), 0, cx + 50, 500, SWP_NOREPOSITION);
+
+
+
+	// 창 투명화 해제
+	pFrame->SetLayeredWindowAttributes(0, 255, LWA_ALPHA);
+
+	// 이미지 뷰에 붙이기
+	Image.BitBlt(dc.m_hDC, 0, 0);
+
+
+
+	// 그 파일을 실행해 준다. 
+
+	/*
+	::ShellExecute(NULL,
+	TEXT("open"), TEXT("Desktop.jpg"),
+	NULL, NULL, SW_SHOW);
+	*/
+}
